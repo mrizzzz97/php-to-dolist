04fc670fc3a287d9aeeaded2972bc4e99e468213
mrizzzz97 <rizkisuryapratama2020@gmail.com>
2026-02-13 09:58:53 +0700

    finishing boy

--- controller/taskController.php
+++ controller/taskController.php
@@ -10,4 +10,32 @@ $stmt = $pdo->query('SELECT * FROM task ORDER BY id DESC');
 $task = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 
+
+/* ======================
+CREATE LOGIC
+======================== */
+
+if(isset($_POST['submit'])){
+    $body = $_POST['body'];
+
+    if(!$body == ""){
+        $stmt = $pdo->prepare("INSERT INTO task (body) VALUES (?)");
+        $stmt-> execute([$body]);
+    }
+
+    header("Location: index.php");
+    exit;
+}
+
+if(isset($_GET['delete'])){
+    $id = (int) $_GET['delete'];
+
+    $stmt = $pdo->prepare("DELETE FROM task WHERE id = ?");
+    $stmt->execute([$id]);
+
+    header("Location: index.php");
+    exit;
+}
+
+
 ?>
--- view/task.php
+++ view/task.php
@@ -10,10 +10,10 @@
 <body class="bg-black flex justify-center">
     <div class="bg-white w-[400px] mt-12 p-4 rounded shadow shadow-slate-300 text-center">
         <p class="text-2xl font-semibold capitalize ">my agenda</p>
-        <form action="">
+        <form action="" method="POST">
             <div class="mt-4 flex gap-2">
-                <input type="text" class="px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-green-400 flex-grow" placeholder="Ketik Disini....">
-                <button type="submit" class="px-5 py-2 bg-green-400 rounded capitalize text-white hover:bg-transparent border border-green-400 hover:text-green-400 transition duration-200">Submit</button>
+                <input type="text" name="body" class="px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-green-400 flex-grow" placeholder="Ketik Disini...." required>
+                <button type="submit" name="submit" class="px-5 py-2 bg-green-400 rounded capitalize text-white hover:bg-transparent border border-green-400 hover:text-green-400 transition duration-200">Submit</button>
             </div>
         </form>
         <ul class="space-y-3 mt-4" >
@@ -21,7 +21,7 @@
                 <?php foreach($task as $task) : ?>
                     <li class="bg-gray-100 p-3 flex items-center text-left gap-4 rounded justify-between ">
                         <span><?= $task['body'] ?> </span>
-                        <a href="" class="bg-red-400 px-3 py-2 rounded text-white hover:bg-red-700 ">Delete</a>
+                        <a href="?delete=<?= $task['id'] ?>?" class="bg-red-400 px-3 py-2 rounded text-white hover:bg-red-700 ">Delete</a>
                     </li>
                 <?php endforeach?>
             <?php else :?>
